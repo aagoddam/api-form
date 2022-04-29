0f6ed5fb892b81d92e5526384c72f9bd54fb0712
unknown <afonin.andrey.plus@icloud.com>
2022-04-29 17:02:07 +0300

    first commit

---
+++ Eureca/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Eureca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(130, 3);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(1022, 22);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Артикул товара";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedVertical;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1528, 709);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.TabStop = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1385, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(1180, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Дата обновления каталога";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1552, 755);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Eureca/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using Npgsql;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Eureca
+{
+    public partial class Form1 : Form
+    {
+        
+        private string id;
+        private NpgsqlConnection con = new NpgsqlConnection("Server=127.0.0.1;Username=postgres;Password=afonin.eureca;Database=eureca");
+        private NpgsqlCommand cmd = new NpgsqlCommand();
+        public Form1()
+        {
+            InitializeComponent();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from eureca.view;";
+
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(rdr);
+                dataGridView1.DataSource = dt;
+
+            }
+           var date = new NpgsqlCommand("select dateofupdate from eureca.item limit 1;",con);
+            var read = date.ExecuteReader();
+            read.Read();
+
+            textBox1.Text = read.GetString(0);
+            cmd.Dispose();
+
+            con.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string sql;
+            
+            if (char.IsLetterOrDigit(e.KeyChar) || e.KeyChar==(char)Keys.Back)
+            {
+                con.Open();
+                string box="";
+                for (int i = 0; i < txtSearch.Text.Length-1; i++) box += txtSearch.Text[i];
+                sql = String.Format("select * from eureca.view Where partnumber like '{0}%';", box);       
+                if (char.IsLetterOrDigit(e.KeyChar))
+                {
+                    sql = String.Format("select * from eureca.view Where partnumber like '{0}%';", txtSearch.Text + e.KeyChar.ToString());
+                }
+                 NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = sql;
+                cmd.CommandType = CommandType.Text;
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(rdr);
+                    dataGridView1.DataSource = dt;
+
+                }
+                con.Close();
+               
+            }
+            if (Char.IsControl (e.KeyChar))
+            {
+                con.Open();
+
+                if (txtSearch.Text.Length == 1)
+                {
+                    sql = "select * from eureca.view;";
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = sql;
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.HasRows)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(rdr);
+                        dataGridView1.DataSource = dt;
+
+                    }
+                }
+
+                
+                
+
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    string[] arr = txtSearch.Text.Split(',');
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = "'" + arr[i] + "'";
+                    }
+                    id = String.Join(",", arr);
+                    sql = string.Format("Select * from eureca.view where partnumber in ({0});", id);
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = sql;
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.HasRows)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(rdr);
+                        dataGridView1.DataSource = dt;
+
+                    }
+                    else MessageBox.Show(String.Format(" По позиции {0} ничего не найдено", id));
+                    cmd.Dispose();
+                }
+                con.Close();
+            }
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
